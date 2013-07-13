d83787d448cc2e22d4b210fce2ac67f045fd8b27
Charles D'Angelo <c.g.dangelo@gmail.com>
2013-07-13 17:37:54 -0400

    Add settings and callbacks

--- eztabs.js
+++ eztabs.js
@@ -1,16 +1,27 @@
 ;(function($) {
-  $.fn.eztabs = function() {
+  $.fn.eztabs = function(options) {
+    var settings = $.extend({}, $.fn.eztabs.defaults, options);
+
     var eztabs = $('[data-eztabs]');
     eztabs.find('[data-toggle]').click(function() {
+      settings.beforeSwitch();
+
       $(this).siblings('[data-toggle]').removeClass('active');
-      $(this).toggleClass('active');
+      $(this).toggleClass(settings.activeClass);
 
       var contentPane = $('[data-tab=' + this.getAttribute('data-toggle') + ']');
       contentPane.siblings('[data-tab]').hide();
       contentPane.toggle();
 
+      settings.afterSwitch();
       return this;
     });
+  }
+
+  $.fn.eztabs.defaults = {
+    activeClass: 'active',
+    beforeSwitch: function() {},
+    afterSwitch: function() {}
   }
 })(jQuery);
 
