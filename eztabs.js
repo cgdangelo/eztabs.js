faa37352c051614460335dd14525aaea878b1ad7
Charles D'Angelo <c.g.dangelo@gmail.com>
2013-07-13 14:44:10 -0400

    Use data attributes for selectors

--- eztabs.js
+++ eztabs.js
@@ -1,11 +1,12 @@
 ;(function($) {
-  $.fn.tabs = function() {
-    $('.tabs > a').click(function() {
-      $(this).siblings('a').removeClass('active');
+  $.fn.eztabs = function() {
+    var eztabs = $('[data-eztabs]');
+    eztabs.find('[data-toggle]').click(function() {
+      $(this).siblings('[data-toggle]').removeClass('active');
       $(this).toggleClass('active');
 
-      var contentPane = $('#' + this.getAttribute('data-toggle'));
-      contentPane.siblings('section').hide();
+      var contentPane = $('[data-tab=' + this.getAttribute('data-toggle') + ']');
+      contentPane.siblings('[data-tab]').hide();
       contentPane.toggle();
 
       return this;
